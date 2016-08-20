24c762b7dfeb1779a7fffd088a726578df0e5e7e
Jeremy M. Reed <reeje76@gmail.com>
2016-08-20 13:54:03 -0600

    Stub out FoodChain functions.

---
+++ food-chain.js
@@ -0,0 +1,14 @@
+var FoodChain = function() {},
+  NotImplementedException = require('./exceptions/NotImplementedException.js'),
+  InvalidParameterException = require('./exceptions/InvalidParameterException.js'),
+  InvalidStateException = require('./exceptions/InvalidStateException.js');
+
+FoodChain.prototype.verse = function(verseNum) {
+  throw new NotImplementedException();
+};
+
+FoodChain.prototype.verses = function(startVerse, endVerse) {
+  throw new NotImplementedException();
+};
+
+module.exports = FoodChain;
